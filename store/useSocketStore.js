33fdd24d2278e0447b24a4f4ebdeb4813e836c0e
Brice Eliasse <brice.eliasse@gmail.com>
2024-06-17 11:33:58 +0200

    env variable for backend address (socket)

--- store/useSocketStore.js
+++ store/useSocketStore.js
@@ -1,11 +1,14 @@
 import { create } from 'zustand';
 import io from 'socket.io-client';
 
+const ADDRESS_BACK = process.env.ADDRESS_BACK || "http://localhost:4000"
+
+
 export const useSocketStore = create((set) => ({
     socket: null,
     connect: () => set((state) => {
         if (!state.socket) {
-            const socket = io('http://localhost:4000'); // Correction de la méthode de connexion
+            const socket = io(ADDRESS_BACK); // Correction de la méthode de connexion
             socket.on('connect_error', (err) => {
                 console.error('Connection Error:', err);
             });
